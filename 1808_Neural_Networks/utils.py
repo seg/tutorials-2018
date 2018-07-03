fcd1e9422a75e851c713b249b572fcde461d8029
kwinkunks <matt@agilegeoscience.com>
2018-07-03 15:55:57 -0300

    for figures

--- 1808_Neural_Networks/utils.py
+++ 1808_Neural_Networks/utils.py
@@ -1,48 +1,35 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# -*- coding: utf-8 -*-
+"""
+Useful functions for writing manuscripts in a Notebook.
+
+:copyright: 2016 Agile Geoscience
+:license: Apache 2.0
+"""
+import os
 
 
-def draw_neural_net(ax, left, right, bottom, top, layer_sizes):
-    '''
-    Draw a neural network cartoon using matplotilb.
-    
-    :usage:
-        >>> fig = plt.figure(figsize=(12, 12))
-        >>> draw_neural_net(fig.gca(), .1, .9, .1, .9, [4, 7, 2])
-    
-    :parameters:
-        - ax : matplotlib.axes.AxesSubplot
-            The axes on which to plot the cartoon (get e.g. by plt.gca())
-        - left : float
-            The center of the leftmost node(s) will be placed here
-        - right : float
-            The center of the rightmost node(s) will be placed here
-        - bottom : float
-            The center of the bottommost node(s) will be placed here
-        - top : float
-            The center of the topmost node(s) will be placed here
-        - layer_sizes : list of int
-            List of layer sizes, including input and output dimensionality
-    '''
-    n_layers = len(layer_sizes)
-    v_spacing = (top - bottom)/float(max(layer_sizes))
-    h_spacing = (right - left)/float(len(layer_sizes) - 1)
-    # Nodes
-    for n, layer_size in enumerate(layer_sizes):
-        layer_top = v_spacing*(layer_size - 1)/2. + (top + bottom)/2.
-        for m in range(layer_size):
-            circle = plt.Circle((n*h_spacing + left, layer_top - m*v_spacing), v_spacing/4.,
-                                color='w', ec='k', zorder=4)
-            ax.add_artist(circle)
-    # Edges
-    for n, (layer_size_a, layer_size_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
-        layer_top_a = v_spacing*(layer_size_a - 1)/2. + (top + bottom)/2.
-        layer_top_b = v_spacing*(layer_size_b - 1)/2. + (top + bottom)/2.
-        for m in range(layer_size_a):
-            for o in range(layer_size_b):
-                line = plt.Line2D([n*h_spacing + left, (n + 1)*h_spacing + left],
-                                  [layer_top_a - m*v_spacing, layer_top_b - o*v_spacing], c='k')
-                ax.add_artist(line)
+class Manuscript:
+    """
+    Something to handle making figures with figure numbers.
+    """
+    def __init__(self, loc=None):
+        self.loc = loc or ''
+        self.fignum = 1
+        self.figures = {}
 
-    return ax
-
+    def savefig(self, figure, name):
+        """
+        Wraps fig.savefig() to automatically keep track of figure numbers.
+        Just pass a name instead when you call it.
+        """
+        if not os.path.exists(self.loc):
+            os.makedirs(self.loc)
+        fname = self.figures.get(name)
+        if fname is not None:
+            figure.savefig(fname, dpi=300)
+        else:
+            f = f"figure_{self.fignum}.png"
+            fname = self.figures.setdefault(name, os.path.join(self.loc, f))
+            figure.savefig(fname, dpi=300)
+            self.fignum += 1
+        return
